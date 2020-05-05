e8c6ca63d9e3b31436b5cbf17cbe4a01a4608687
rnash13 <54478446+rnash13@users.noreply.github.com>
2020-05-04 20:44:35 -0500

    rank map updated to get correct file path

--- include/CARD.h
+++ include/CARD.h
@@ -27,9 +27,9 @@ enum SUIT_ENUM {
 };
 
 const std::map<int, std::string> RANK_STRINGS =	{
-    {2, "Two"}, {3, "Three"}, {4, "Four"},
-    {5, "Five"}, {6, "Six"}, {7, "Seven"},
-    {8, "Eight"}, {9, "Nine"}, {10, "Ten"},
+    {2, "2"}, {3, "3"}, {4, "4"},
+    {5, "5"}, {6, "6"}, {7, "7"},
+    {8, "8"}, {9, "9"}, {10, "10"},
     {11, "Jack"}, {12, "Queen"},
     {13, "King"}, {1, "Ace"}
 };
